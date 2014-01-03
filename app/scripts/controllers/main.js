688bb6602193b3c7c764b80b1b509b8f98aa894d
dracula <dracula@test.com>
2014-01-02 23:13:40 -0800

    working mainpage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('snapchatApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+    .controller('MainCtrl', function($scope, $http) {
+        $http.get('/api/byPhone/2-1212122').success(function(awesomeThings) {
+            $scope.awesomeThings = awesomeThings;
+        });
     });
-  });
