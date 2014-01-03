6ff87304d08f8ecb3e24ce618a973ab008043190
dracula <dracula@test.com>
2014-01-02 22:57:19 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Module dependencies.
+var express = require('express');
+
+var app = express();
+
+
+// Express Configuration
+require('./lib/config/express')(app);
+
+// Controllers
+var api = require('./lib/controllers/api'),
+    index = require('./lib/controllers');
+
+// Server Routes
+app.get('/api/byPhone/:num', api.phone);
+
+// Angular Routes
+app.get('/partials/*', index.partials);
+app.get('/*', index.index);
+
+// Start server
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('Express server listening on port %d in %s mode', port, app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;
